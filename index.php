b4d1eac6b7941bd4874ef10dddeb3f75c88e3f9c
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-04 00:10:05 +0800

    Removed CSS

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   <head>
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="stylesheet" href="./assets/style.css" />
     <script src="assets/main.js"></script>
     
     <title>E-Commerce Demo</title>
