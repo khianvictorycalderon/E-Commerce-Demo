dde41d8d6ef90ac7a80a6583099d9a600bcd619f
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-04 18:27:59 +0800

    Removed comment

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="/images/e-commerce-demo.png">
 
-    <!-- Tailwind should not be a defer -->
     <script src="assets/tailwind-3.4.17.js"></script>
-
-    <!-- Defer scripts -->
     <script type="module" src="assets/main.js"></script>
     
     <title>E-Commerce Demo</title>
